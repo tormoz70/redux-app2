94001a352d965e704398308846f6bcafb62f1bb7
Ayrat <gpsdom@gmail.com>
2025-03-17 22:07:23 +0300

    lesson-02

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { DecrimentAction, IncrimentAction, store } from './store'
-import { useEffect, useReducer } from 'react'
+import { AppState, CounterId, DecrimentAction, IncrimentAction, store } from './store'
+import { useEffect, useReducer, useRef } from 'react'
 
 function App() {
 
-  const [, forceUpdate] = useReducer((x) => x + 1, 0)
-
-  useEffect(() => {
-    const unsubsribe = store.subscribe(() => {
-      forceUpdate()
-    })
-    return unsubsribe
-  })
 
   return (
     <>
@@ -27,13 +19,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <div>counter: {store.getState().counter}</div>
-        <button onClick={() => store.dispatch({ type: "incriment" } satisfies IncrimentAction)}>
-          incriment
-        </button>
-        <button onClick={() => store.dispatch({ type: "decriment" } satisfies DecrimentAction)}>
-          decriment
-        </button>
+        <Counter counterId='first'/>
+        <Counter counterId='second'/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -43,6 +30,42 @@ function App() {
       </p>
     </>
   )
+}
+
+const selectCounter = (state: AppState, counterId: CounterId) => state.counters[counterId]
+
+export function Counter({counterId}: { counterId: CounterId }) {
+  const [, forceUpdate] = useReducer((x) => x + 1, 0)
+  console.log("render counter: ", counterId)
+
+  const lastStateRef = useRef<ReturnType<typeof selectCounter>>(undefined)
+
+  useEffect(() => {
+    const unsubsribe = store.subscribe(() => {
+      const currentState = selectCounter(store.getState(), counterId)
+      const lastState = lastStateRef.current
+      if(currentState !== lastState){
+        forceUpdate()
+      }
+      lastStateRef.current = currentState
+    })
+    return unsubsribe
+  })
+
+  const counterState = selectCounter(store.getState(), counterId)
+
+  return (
+    <>   
+        <div>counter({ counterId }): {counterState?.counter}</div>
+        <button onClick={() => store.dispatch({ type: "incriment", payload: { counterId } } satisfies IncrimentAction)}>
+          incriment
+        </button>
+        <button onClick={() => store.dispatch({ type: "decriment", payload: { counterId } } satisfies DecrimentAction)}>
+          decriment
+        </button>
+
+    </>
+  );
 }
 
 export default App
--- src/store.ts
+++ src/store.ts
@@ -1,36 +1,69 @@
 import { configureStore } from "@reduxjs/toolkit"
 
-type State = {
+type CounterState = {
     counter: number;
+}
+export type CounterId = string
+
+type State = {
+    counters: Record<CounterId, CounterState | undefined>;
 }
 
 
 export type IncrimentAction = {
     type: "incriment"
+    payload: {
+        counterId: CounterId
+    }
 }
 
 export type DecrimentAction = {
     type: "decriment"
+    payload: {
+        counterId: CounterId
+    }
 }
 
 type Action = IncrimentAction | DecrimentAction
 
-const initialState: State = {
+const initialCounterState: CounterState = {
     counter: 0
+} 
+
+const initialState: State = {
+    counters: {}
 } 
 
 const reducer = (state: State = initialState, action: Action): State => {
     switch (action.type) {
-        case "incriment":
+        case "incriment": {
+            const { counterId } = action.payload
+            const currentCounter = state.counters[counterId] ?? initialCounterState
             return {
                 ...state,
-                counter: state.counter + 1
+                counters: {
+                    ...state.counters,
+                    [counterId]: {
+                        ...currentCounter,
+                        counter: currentCounter.counter + 1
+                    }
+                }
             }    
-        case "decriment":
+        }
+        case "decriment":{
+            const { counterId } = action.payload
+            const currentCounter = state.counters[counterId] ?? initialCounterState
             return {
                 ...state,
-                counter: state.counter - 1
+                counters: {
+                    ...state.counters,
+                    [counterId]: {
+                        ...currentCounter,
+                        counter: currentCounter.counter - 1
+                    }
+                }
             }    
+        }
         default:
             return state
     }
@@ -39,3 +72,5 @@ const reducer = (state: State = initialState, action: Action): State => {
 export const store = configureStore({
     reducer: reducer
 })
+
+export type AppState = ReturnType<typeof store.getState>
